63b9fad78a1ee70cd8a752f7028f0ddfed52c20f
Tasnem <tasnem@kth.se>
2026-09-03 08:38:42 +0000

    dubletter.py main.py

--- main.py
+++ main.py
@@ -21,3 +21,19 @@ def main():
     searches(tree)
 
 main()
+
+
+#uppgift 2
+from bintreeFile import Bintree
+svenska = Bintree()
+with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
+    for rad in svenskfil:
+        ordet = rad.strip()                # Ett trebokstavsord per rad
+        if ordet in svenska:
+            print(ordet, end = " ") 
+        else:
+            svenska.put(ordet)             # in i sökträdet
+print("\n") 
+
+
+#uppgift 3
